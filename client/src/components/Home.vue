6eea327bba4e056541dd51272d5a99a55a7a75b0
CHIPCHIP <79151948+yisu0128@users.noreply.github.com>
2023-08-19 13:53:07 +0900

    second-commit

---
+++ client/src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home">
+    <div class="jumbotron text-center">
+      <h1 class="display-4">FANNINGK</h1>
+    </div>
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="input-group mb-3">
+            <input class="form-control" type="text" placeholder="Search" v-model="searchQuery">
+            <div class="input-group-append">
+              <button class="btn btn-primary" @click="search">Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'FANNINGK',
+      searchQuery: ''
+    };
+  },
+  methods: {
+    search() {
+      console.log('Search button clicked! Search query:', this.searchQuery);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.home {
+  background-color: #0B3B2E;
+  color: white;
+  font-family: Georgia, serif;
+  height: 100vh;
+}
+
+.jumbotron {
+  background-color: transparent;
+  padding: 100px 0;
+  margin-bottom: 0;
+}
+
+.container {
+  margin-top: 50px;
+}
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'; // 부트스트랩 스타일 추가
+import 'bootstrap/dist/js/bootstrap.js'; // 부트스트랩 스크립트 추가
+
 
 const app = createApp(App)
 
